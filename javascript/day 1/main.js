cb48739814d74b7056b895bcadbc6ebde4894e2f
Rahul Khilari <149717670+Rahul0792@users.noreply.github.com>
2024-12-02 17:52:29 +0530

    first day practice

---
+++ javascript/day 1/main.js
@@ -0,0 +1,16 @@
+console.log("rahul khilari");
+var age = 30;
+console.log(age);
+
+var username = "Rahul";
+console.log(username);
+
+   
+let cityName = "Shivajinagar";
+let mathMarks = 35;
+console.log(mathMarks);
+const userId = 123456;
+
+console.log(typeof userId);
+
+
